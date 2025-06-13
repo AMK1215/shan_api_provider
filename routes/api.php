387d1565eb9f567ec4c39bc9f138227ca46dbf8f
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-13 12:17:45 +0630

    client

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // shan api provider
 Route::group(['prefix' => 'shan'], function () {
-    Route::get('balance', [ShanGetBalanceController::class, 'shangetbalance']);
+    Route::post('balance', [ShanGetBalanceController::class, 'shangetbalance']);
 });
