12075268fc5030f9011df62faecdae63e2a15cfb
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-13 12:04:35 +0630

    client

--- database/seeders/OperatorSeeder.php
+++ database/seeders/OperatorSeeder.php
@@ -18,8 +18,8 @@ class OperatorSeeder extends Seeder
 
         // You can add more operators here
         Operator::create([
-            'code' => 'a3h1',
-            'secret_key' => 'shana3h1',
+            'code' => 'a3h2',
+            'secret_key' => 'shana3h2',
             'callback_url' => 'https://a1yoma.online/api/launch-game',
             'active' => true,
         ]);
