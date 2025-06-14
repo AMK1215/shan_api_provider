c29100c4075345a98f6a32261ee2cc75f2ad4b77
AMK1215 <aungmyokhaing1215@gmail.com>
2025-06-14 08:14:17 +0630

    provider launchgame

--- app/Http/Controllers/Api/V1/ShanLaunchGameController.php
+++ app/Http/Controllers/Api/V1/ShanLaunchGameController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User; // Adjust as needed
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Http;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class ShanLaunchGameController extends Controller
 {
@@ -14,6 +16,7 @@ class ShanLaunchGameController extends Controller
         // Validate input
         $validator = Validator::make($request->all(), [
             'member_account' => 'required|string|max:50',
+            'operator_code'  => 'required|string', // For security and correct sign
         ]);
 
         if ($validator->fails()) {
@@ -24,19 +27,72 @@ class ShanLaunchGameController extends Controller
             ], 422);
         }
 
-        $user = User::with('wallet')->where('user_name', $request->member_account)->first();
-        if (!$user || !$user->wallet) {
-            return response()->json([
-                'status' => 'fail',
-                'message' => 'Member not found or wallet not available',
-            ], 404);
+        $member_account = $request->member_account;
+        $operator_code = $request->operator_code;
+        $currency = 'MMK';
+        $request_time = time();
+        $secret_key = env('SEAMLESS_SECRET_KEY', ''); // Or get from Operator table if multi-operator
+        $sign = md5($operator_code . $request_time . 'getbalance' . $secret_key);
+
+        $getBalancePayload = [
+            'batch_requests' => [
+                [
+                    'member_account' => $member_account,
+                    'product_code'   => 1002, // or as appropriate
+                ]
+            ],
+            'operator_code' => $operator_code,
+            'currency'      => $currency,
+            'request_time'  => $request_time,
+            'sign'          => $sign,
+        ];
+
+        // 1. Call GetBalance API (internal call)
+        $getBalanceApiUrl = url('/api/shan/balance');
+        $response = Http::post($getBalanceApiUrl, $getBalancePayload);
+
+        $resultData = $response->json();
+        $balance = 0;
+        $needCreate = false;
+
+        if (isset($resultData['data'][0]['code']) && $resultData['data'][0]['code'] == 998) {
+            // Member not found (use your SeamlessWalletCode for "MemberNotExist")
+            $needCreate = true;
+        } elseif (isset($resultData['data'][0]['balance'])) {
+            $balance = $resultData['data'][0]['balance'];
         }
 
-        // Get current balance
-        $balance = (int) $user->wallet->balanceFloat; // or round as needed
+        // 2. If member doesn't exist, create them
+        if ($needCreate) {
+            DB::beginTransaction();
+            $user = User::where('member_account', $member_account)->first();
+            if (!$user) {
+                $user = User::create([
+                    'member_account' => $member_account,
+                    'name'           => $member_account,
+                    'balance'        => 0,
+                    'password'       => bcrypt('defaultpassword'),
+                    'register_date'  => now(),
+                ]);
+            }
+            DB::commit();
 
-        // Build launch game URL
-        $launchGameUrl = 'https://goldendragon7.pro/?user_name=' . urlencode($request->member_account) . '&balance=' . $balance;
+            // 3. Call GetBalance API again for this new member
+            $request_time = time();
+            $sign = md5($operator_code . $request_time . 'getbalance' . $secret_key);
+            $getBalancePayload['request_time'] = $request_time;
+            $getBalancePayload['sign'] = $sign;
+
+            $response = Http::post($getBalanceApiUrl, $getBalancePayload);
+            $resultData = $response->json();
+            $balance = 0;
+            if (isset($resultData['data'][0]['balance'])) {
+                $balance = $resultData['data'][0]['balance'];
+            }
+        }
+
+        // 4. Build launch game URL
+        $launchGameUrl = 'https://goldendragon7.pro/?member_account=' . urlencode($member_account) . '&balance=' . $balance;
 
         return response()->json([
             'status' => 'success',
